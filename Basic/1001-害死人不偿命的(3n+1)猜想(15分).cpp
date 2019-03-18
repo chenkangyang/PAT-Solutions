9a98c179ad153a13edb8e1b1aaad521a739841df
chenkangyang <chenkangyang98@gmail.com>
2019-03-18 22:57:23 +0800

    建立仓库

---
+++ Basic/1001-害死人不偿命的(3n+1)猜想(15分).cpp
@@ -0,0 +1,12 @@
+//
+// Created by 陈康扬 on 2019-03-18.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"helle world\n";
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/1001.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1001.dir/Basic/1001-害死人不偿命的(3n+1)猜想(15分).cpp.o"
+  "1001.pdb"
+  "1001"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1001.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
